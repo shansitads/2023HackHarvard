7c448c0a876c7f1698d9a6e9d2b0deb8b817e033
stevejj9403 <stevejung394@gmail.com>
2023-10-21 12:09:59 -0500

    signup and login auth

--- src/backend/firebase.jsx
+++ src/backend/firebase.jsx
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app";
 import {
-    getFirestore, collection, query, setDoc, getDoc, getDocs, doc, deleteDoc, loadBundle, where
+    getFirestore, setDoc, doc
 } from 'firebase/firestore';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signOut,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from 'firebase/auth'
 
 import {data} from "./keys.jsx"
 
@@ -17,5 +25,34 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
 
-export default {app, db};
+function createUser(email, password, careTakerName, careTakerEmail) {
+  createUserWithEmailAndPassword(auth, email, password).then((cred)=> {
+    setDoc(doc(db, 'users', cred.user.uid), {
+      careTakerName: careTakerName,
+      careTakerEmail: careTakerEmail
+    })
+  });
+}
+
+function login(email, password) {
+  signInWithEmailAndPassword(auth, email, password).then((cred)=> {
+    //reset the text fields here.
+  })
+  .catch((err)=> {
+    //Need to add frontend information to notify user login fail
+    console.log("login failed");
+  })
+}
+
+function logout() {
+  signOut(auth).then(()=> {
+    console.log("the user signed out");
+  })
+  .catch((err)=> {
+    console.log(err.message);
+  })
+}
+
+export default {app, db, createUser, login, logout};
