792e44e46c683d1315c6abca1f928e4df4111d02
John Lee <2003jjl@gmail.com>
2023-10-21 01:51:39 -0400

    basic login screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginForm from './components/LoginForm'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [seen, setSeen] = useState(false);
+  const [formType, setFormType] = useState("login");
+
+  function togglePop () {
+    setSeen(!seen);
+  };
+
+  function switchFormType(type) {
+    setFormType(type);
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className='popup'>
+          <button
+            onClick={() => {
+              togglePop();
+              switchFormType("login");
+            }}
+          >
+            Login
+          </button>
+          <button
+            onClick={() => {
+              togglePop();
+              switchFormType("signup");
+            }}
+          >
+            Sign Up
+          </button>
+          {seen ? (
+            formType === "login" ? (
+              <LoginForm toggle={togglePop} />
+            ) : (
+              <SignUpForm toggle={togglePop} />
+            )
+          ) : null}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
