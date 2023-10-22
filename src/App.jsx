fd981e434a81f7c5826430e4a8988ed830aba9a6
shansitads <shansitads@gmail.com>
2023-10-21 22:49:12 -0400

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginAndSignup from './pages/LoginAndSignup.jsx'
-import SleepingScaleGame from './pages/SleepinessScaleGame.jsx'
+import LoginAndSignup from './pages/LoginAndSignup'
+import SleepingScaleGame from './pages/SleepinessScaleGame'
+import MoodTracker from './pages/MoodTracker'
+import ImageDifferenceFame from './pages/ImageDifferenceGame'
+import ImageIdentificationGame from './pages/ImageIdentificationGame'
+import ReactionTimeGame from './pages/ReactionTimeGame'
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const dataRef = useRef(null)
 
-  return (  
+  return (
     <BrowserRouter>
-      <LoginAndSignup dataRef={dataRef} />
+      <Routes>
+        <Route path="/" element={<LoginAndSignup dataRef={dataRef} />}>
+          <Route path="moodtracker" element={<MoodTracker />} />
+          <Route path="sleepingScale" element={<SleepingScaleGame />} />
+          <Route path="whackamole" element={<ReactionTimeGame />} />
+          <Route path="imageDifference" element={<ImageDifferenceFame />} />
+          <Route path="newImageIdentify" element={<ImageIdentificationGame />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
